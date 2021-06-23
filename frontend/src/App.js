63e5fa066a1abd24c9d5b032ddcb1b3b6b8b1086
mmurphy-1 <64709774+mmurphy-1@users.noreply.github.com>
2021-06-23 13:30:13 +0100

    Created components directory to store all project components, created a Header and Footer component and added them to the App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div>
+            <Header />
+            <h1>Hello world, welcome to the Fabrique app!</h1>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
