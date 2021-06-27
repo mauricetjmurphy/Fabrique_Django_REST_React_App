7930e40e63bddc3d967f9dc6042778202830df58
mmurphy-1 <64709774+mmurphy-1@users.noreply.github.com>
2021-06-27 14:19:00 +0100

    Created the djange database classes with all required attributes

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,69 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.fields.files import ImageField
 # Create your models here.
+
+
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    product_id = models.AutoField(primary_key=True, editable=False)
+    product_name = models.CharField(max_length=200, null=True, blank=True)
+    availability = models.CharField(max_length=200, null=True, blank=True)
+    product_category = models.CharField(max_length=200, null=True, blank=True)
+    brand = models.CharField(max_length=200, null=True, blank=True)
+    color =models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField( null=True, blank=True)
+    gender = models.CharField(max_length=200, null=True, blank=True)
+    size = models.CharField(max_length=200, null=True, blank=True)
+    material = models.CharField(max_length=200, null=True, blank=True)
+    retail_price = models.CharField(max_length=200, null=True, blank=True)
+    product_url = models.CharField(max_length=200, null=True, blank=True)
+    product_image_url = models.TextField( null=True, blank=True)
+    additional_image_link = models.TextField( null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.product_name)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    paymentMethod = models.CharField(max_length=200, null=True, blank=True)
+    taxPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    totalPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    isPaid = models.BooleanField(default=False)
+    paidOn = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    isDelivered = models.BooleanField(default=False)
+    deliveredOn = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    createdOn = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.createdOn)
+
+
+class OrderItem(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class ShippingAddress(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    postalCode = models.CharField(max_length=200, null=True, blank=True)
+    country = models.CharField(max_length=200, null=True, blank=True)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.address)
