824fb9f799672130201aadb5afebe1b7f6adfe96
mmurphy-1 <64709774+mmurphy-1@users.noreply.github.com>
2021-07-19 19:05:37 +0100

    Created the user register action function

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,9 @@ import {
     USER_LOGIN_SUCCESS,
     USER_LOGIN_FAIL,
     USER_LOGOUT,
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL,
 } from "../constants/userConstants";
 import axios from "axios";
 
@@ -50,4 +53,46 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
     localStorage.removeItem("userInfo");
     dispatch({ type: USER_LOGOUT });
+};
+
+export const Register = (name, email, password) => async (dispatch) => {
+    try {
+        // Dispatch contains an object that describes what action needs to take place. The dispatch function then dispatches that action.
+        dispatch({
+            type: USER_REGISTER_REQUEST,
+        });
+
+        // Axios post request will require a header. This variable is passed in below.
+        const config = {
+            header: {
+                "Content-type": "application/json",
+            },
+        };
+
+        //Login request that is looking for a web token to be returned. Sends the username and password and gets a token in return.
+        const { data } = await axios.post(
+            "/api/users/register/",
+            {
+                name: name,
+                username: email,
+                password: password,
+            },
+            config
+        );
+
+        dispatch({
+            type: USER_REGISTER_SUCCESS,
+            payload: data,
+        });
+
+        localStorage.setItem("userInfo", JSON.stringify(data));
+    } catch (error) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload:
+                error.response && error.response.data.detail
+                    ? error.response.data.detail
+                    : error.message,
+        });
+    }
 };
