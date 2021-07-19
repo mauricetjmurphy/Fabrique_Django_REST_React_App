7d1c59fa01815d6085edf7f6be97ff8a82e47114
mmurphy-1 <64709774+mmurphy-1@users.noreply.github.com>
2021-07-19 12:53:50 +0100

    Cretaed a new env and removed the old one beacuse of issues with my python version, created the user login in constants, reducer and action functions and files.

---
+++ frontend/src/reducers/userReducers.js
@@ -0,0 +1,29 @@
+import {
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGIN_FAIL,
+    USER_LOGOUT,
+} from "../constants/userConstants";
+
+// The reducer does not modify the state, it creates a new state object that is merged merged with the old one state object.
+export const userLoginReducer = (state = {}, action) => {
+    switch (action.type) {
+        // Do something here based on the different types of actions
+        case USER_LOGIN_REQUEST:
+            return { loading: true };
+
+        case USER_LOGIN_SUCCESS:
+            // action.payload is the data returned form the API call
+            return { loading: false, userInfo: action.payload };
+
+        case USER_LOGIN_FAIL:
+            return { loading: false, error: action.payload };
+
+        case USER_LOGOUT:
+            return {};
+        // If this reducer doesn't recognize the action type, or doesn't
+        // care about this specific action, return the existing state unchanged
+        default:
+            return state;
+    }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,11 +6,13 @@ import {
     productDetailsReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducers = {
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 };
 
 const rootReducer = combineReducers(reducers);
@@ -19,8 +21,13 @@ const cartItemsFromStorage = localStorage.getItem("cartItems")
     ? JSON.parse(localStorage.getItem("cartItems"))
     : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null;
+
 const initialState = {
     cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage },
 };
 const middleware = [thunk];
 
