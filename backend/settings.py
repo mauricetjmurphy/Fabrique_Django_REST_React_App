0b17ffc044420de7b7ec63bc47837d0f0825d021
mmurphy-1 <64709774+mmurphy-1@users.noreply.github.com>
2021-08-15 10:06:14 +0100

    Updated the settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','fabrique-django-react-app.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','fabrique-django-react-app.herokuapp.com']
 
 
 # Application definition
