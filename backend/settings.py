5efa021ae6ef7a9b826bdb4e3b20a66f3a097856
mmurphy-1 <64709774+mmurphy-1@users.noreply.github.com>
2021-08-15 10:02:50 +0100

    Updated the settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','fabrique-django-react-app.herokuapp.com/']
 
 
 # Application definition
